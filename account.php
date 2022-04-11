b7120250861c9d322c653b92caaf73bed07ea70e
JuaquinFranco <juaquin.gutierrez02@gmail.com>
2022-04-11 22:14:46 +0800

    php login script added

--- account.php
+++ account.php
@@ -1,3 +1,21 @@
+<?php
+
+SESSION_START();
+
+if (isset($_SESSION['ses_username'])===false){
+    header("Location: index.php?logfirst");
+
+}
+        elseif (isset($_REQUEST['logout'])===true){
+            session_destroy();
+            header("Location: index.php?logout");
+        }
+
+
+?>
+
+
+
 <!doctype html>
                         <html>
                             <head>
@@ -32,13 +50,17 @@ body {
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="images/pfp.png" alt="..." height="100" width="150" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit Profile</a>
+                    <div class="profile mr-3"><img src="images/pfp.png" alt="..." height="100" width="150" class="rounded mb-2 img-thumbnail">
+
+                        <a href="?logout" class="btn btn-outline-dark btn-sm btn-block">Sign Out</a>
 
 
                     </div>
                     <div class="media-body mb-5 text-white">
-                        <h4 class="mt-0 mb-0" >Juaquin Franco Gutierrez</h4>
-                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>Marinduque, PH
+                        <h4 class="mt-0 mb-0" ><?php echo $_SESSION['ses_fullname']; ?> </h4>
+                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>
+
+                                    <?php echo $_SESSION['ses_address']; ?>
                          </p>
                     </div>
                 </div>
